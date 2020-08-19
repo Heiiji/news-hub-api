4b765a7c5c3acd0aa2ce78a7e019c5c6be8c7538
Julien <julien.juret@gmail.com>
2020-08-19 12:04:31 +0200

    POC

---
+++ config/environments/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "127.0.0.1",
+    port: 5000,
+    mongoUrl: "mongodb+srv://heiji:An4lCfEIzU5JGAHF@cluster0-u3ymg.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    logLevel: "debug", // can be chenged to error, warning, info, verbose or silly
+    secret: "NierAutomata"
+}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { ApolloServer } = require('apollo-server');
 
+const config = require("./config");
+
+const port = process.env.PORT || config.port;
+const ENV = process.env.NODE_ENV || config.env;
+
 const typeDefs = `
     type Query {
         hello: String!
@@ -12,11 +17,15 @@ const resolvers = {
     }
 }
 
-const server = new ApolloServer({
+const app = new ApolloServer({
     typeDefs,
     resolvers
 })
 
-server.listen(80).then(({url}) => {
+app.set( "env", ENV );
+
+require( "./config/mongoose" )( app );
+
+app.listen(port).then(({url}) => {
     console.log("l'api tourne sur ", url);
 });
