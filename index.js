f3fbc7b4dfb3628c822f6673ec70509475d45564
Julien Juret <julien.juret@gmail.com>
2021-06-27 19:15:40 +0200

    lint + prettier

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const cors = require('cors');
-const dotEnv = require('dotenv');
-const cron = require('node-cron');
-const routine = require('./utils/routine');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const cors = require("cors");
+const dotEnv = require("dotenv");
+const cron = require("node-cron");
+const routine = require("./utils/routine");
 
-const resolvers = require('./resolvers');
-const typeDefs = require('./typeDefs');
-const { connection } = require('./database/util');
-const { verifyUser } = require('./helper/context');
+const resolvers = require("./resolvers");
+const typeDefs = require("./typeDefs");
+const { connection } = require("./database/util");
+const { verifyUser } = require("./helper/context");
 
 // set env var
 
@@ -28,33 +28,32 @@ app.use(cors());
 app.use(express.json());
 
 const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-        await verifyUser(req);
-        return {
-            email: req.email
-        }
-    }
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    await verifyUser(req);
+    return {
+      email: req.email,
+    };
+  },
 });
 
-apolloServer.applyMiddleware({app, path: "/graphql"});
+apolloServer.applyMiddleware({ app, path: "/graphql" });
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/", (req, res, next) => {
-    res.send("Hello World");
-})
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-    console.log(`server listen on PORT : ${PORT}`);
-    console.log(`graphql endpoint  : ${apolloServer.graphqlPath}`);
-})
-
+  console.log(`server listen on PORT : ${PORT}`);
+  console.log(`graphql endpoint  : ${apolloServer.graphqlPath}`);
+});
 
 // routine
 
-cron.schedule("*/60 * * * *", function() {
-    console.log("Routine start");
-    routine();
-  });
+cron.schedule("*/59 * * * *", function () {
+  console.log("Routine start");
+  routine();
+});
